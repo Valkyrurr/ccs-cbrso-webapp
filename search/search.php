07669e890e2a11175af45bd252525926f3a3cd5d
Valkyrurr <efstorralba@gmail.com>
2016-05-24 16:54:30 +0800

    Added server-side check for 'keyword' input

--- search/search.php
+++ search/search.php
@@ -37,68 +37,72 @@ if (isset ($_GET)) {
 	$keyword = trim($_GET ['keyword']);
 	$category = $_GET['category'];
 	
-	$keywords = explode(" ", $keyword);
-	foreach($keywords as $key => $value){
-		$keywords[$key] = "+" . $value . "*";
-	}
-	$keyword = implode(" ", $keywords);
-	
-	$dbh = new Database ();
-	$defaultsqlHead = "SELECT themes.`theme` AS theme, areas.`area` AS area, titles.`title` AS title, CONCAT(teachers.`first_name`, ' ', teachers.`middle_name`, ' ', teachers.`last_name`) AS teacher, GROUP_CONCAT(CONCAT(students.`first_name`, IF(students.middle_name IS NULL, '', CONCAT(' ', students.middle_name)), ' ', students.`last_name`, IF(students.ext IS NULL, '', CONCAT(', ', students.ext))) SEPARATOR ';') AS student
-		FROM root
-		LEFT JOIN themes ON themes.id=root.theme_id
-		LEFT JOIN areas ON areas.id=root.area_id
-		INNER JOIN titles ON titles.id=root.title_id
-		INNER JOIN teachers ON teachers.id=root.teacher_id
-		INNER JOIN students ON students.id=root.student_id ";
-	$defaultsqlFoot = " GROUP BY theme, area, title, teacher;";
-	
-	foreach($categories as $key => $value){
-		if($category == $value){
-			if($value == 4 OR $value == 5){
-				$sql = "WHERE MATCH(".$key."s.first_name, ".$key."s.middle_name, ".$key."s.last_name) AGAINST(:keyword IN BOOLEAN MODE)";	
-			} else {
-				$sql = "WHERE MATCH(".$key."s.".$key.") AGAINST(:keyword IN BOOLEAN MODE)";
-			}
+	if(strlen($keyword) < 3){
+		echo "Input minimum length is 3.";
+	} else {
+		$keywords = explode(" ", $keyword);
+		foreach($keywords as $key => $value){
+			$keywords[$key] = "+" . $value . "*";
 		}
-	}
-	
-	$stmt = $dbh->prepare($defaultsqlHead . $sql . $defaultsqlFoot);
-	$stmt->execute(array(":keyword" => $keyword));
-	$rows = $stmt->fetchAll();
-	echo "Found " . $stmt->rowCount() . " results.";
-	echo "<table id='sortthis' class='tablesorter'><thead><tr><th>$category_headers[0]</th><th>$category_headers[1]</th><th>$category_headers[2]</th><th>$category_headers[3]</th><th>$category_headers[4]</th></tr></thead>";
-	echo "<tbody>";
-	foreach ($rows as $row){
-		$adviser = explode(" ", $row['teacher']);
-		foreach($adviser as $key => $value){
-			if(strlen($value) == 1){
-				$adviser[$key] = $value . ".";
-			}
-		}
-		$adviser = implode(" ", $adviser);
-		$students = explode(";", $row['student']);
-		foreach($students as $student => $name){	//student names
-			$name = explode(" ", $name);	// student: Eriko Florenze S Torralba
-			foreach($name as $key => $value){	// 0 => Eriko, 1 => Florenze, 2 => S, 3 => Torralba
-				if(strlen($value) == 1){
-					$name[$key] = $value . ".";
+		$keyword = implode(" ", $keywords);
+		
+		$dbh = new Database ();
+		$defaultsqlHead = "SELECT themes.`theme` AS theme, areas.`area` AS area, titles.`title` AS title, CONCAT(teachers.`first_name`, ' ', teachers.`middle_name`, ' ', teachers.`last_name`) AS teacher, GROUP_CONCAT(CONCAT(students.`first_name`, IF(students.middle_name IS NULL, '', CONCAT(' ', students.middle_name)), ' ', students.`last_name`, IF(students.ext IS NULL, '', CONCAT(', ', students.ext))) SEPARATOR ';') AS student
+			FROM root
+			LEFT JOIN themes ON themes.id=root.theme_id
+			LEFT JOIN areas ON areas.id=root.area_id
+			INNER JOIN titles ON titles.id=root.title_id
+			INNER JOIN teachers ON teachers.id=root.teacher_id
+			INNER JOIN students ON students.id=root.student_id ";
+		$defaultsqlFoot = " GROUP BY theme, area, title, teacher;";
+		
+		foreach($categories as $key => $value){
+			if($category == $value){
+				if($value == 4 OR $value == 5){
+					$sql = "WHERE MATCH(".$key."s.first_name, ".$key."s.middle_name, ".$key."s.last_name) AGAINST(:keyword IN BOOLEAN MODE)";	
+				} else {
+					$sql = "WHERE MATCH(".$key."s.".$key.") AGAINST(:keyword IN BOOLEAN MODE)";
 				}
 			}
-			$name = implode(" ", $name);
-			$students[$student] = $name;
 		}
-		echo "<tr>";
-		echo "<td>" . $row['theme'] . "</td><td>" . $row['area'] . "</td><td>" . $row['title'] . "</td><td>" . $adviser . "</td>";
-		echo "<td><table id='sortthat'><thead><tr><th></th></tr></thead><tbody>";
-		foreach($students as $student){
-			echo "<tr><td>" . $student . "</td></tr>"; 
+		
+		$stmt = $dbh->prepare($defaultsqlHead . $sql . $defaultsqlFoot);
+		$stmt->execute(array(":keyword" => $keyword));
+		$rows = $stmt->fetchAll();
+		echo "Found " . $stmt->rowCount() . " results.";
+		echo "<table id='sortthis' class='tablesorter'><thead><tr><th>$category_headers[0]</th><th>$category_headers[1]</th><th>$category_headers[2]</th><th>$category_headers[3]</th><th>$category_headers[4]</th></tr></thead>";
+		echo "<tbody>";
+		foreach ($rows as $row){
+			$adviser = explode(" ", $row['teacher']);
+			foreach($adviser as $key => $value){
+				if(strlen($value) == 1){
+					$adviser[$key] = $value . ".";
+				}
+			}
+			$adviser = implode(" ", $adviser);
+			$students = explode(";", $row['student']);
+			foreach($students as $student => $name){	//student names
+				$name = explode(" ", $name);	// student: Eriko Florenze S Torralba
+				foreach($name as $key => $value){	// 0 => Eriko, 1 => Florenze, 2 => S, 3 => Torralba
+					if(strlen($value) == 1){
+						$name[$key] = $value . ".";
+					}
+				}
+				$name = implode(" ", $name);
+				$students[$student] = $name;
+			}
+			echo "<tr>";
+			echo "<td>" . $row['theme'] . "</td><td>" . $row['area'] . "</td><td>" . $row['title'] . "</td><td>" . $adviser . "</td>";
+			echo "<td><table id='sortthat'><thead><tr><th></th></tr></thead><tbody>";
+			foreach($students as $student){
+				echo "<tr><td>" . $student . "</td></tr>"; 
+			}
+			echo "</tbody></table></td>";
+			echo "</tr>";
 		}
-		echo "</tbody></table></td>";
-		echo "</tr>";
+		echo "</tbody></table>";
+		echo "Found " . $stmt->rowCount() . " results.";
 	}
-	echo "</tbody></table>";
-	echo "Found " . $stmt->rowCount() . " results.";
 }
 ?>
 </div>
